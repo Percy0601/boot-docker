cb6b0a5934fa071a65a0b6d45e56aef8da302063
Percy <2008dreams@2008.sina.com>
2021-10-30 18:15:57 +0800

    临时提交

--- src/main/java/io/training/boot/docker/TrainingController.java
+++ src/main/java/io/training/boot/docker/TrainingController.java
@@ -1,7 +1,6 @@
 package io.training.boot.docker;
 
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ public class TrainingController {
         if(ObjectUtils.isEmpty(name)) {
             name = "Percy";
         }
-        return "Hello " + name + "!" + " Now:" + new SimpleDateFormat(" [yyyy-MM-dd  HH:mm:ss]").format(new Date());
+        return "Hello " + name + "!" + " Now:" + new SimpleDateFormat(" [yyyy-MM-dd HH:mm:ss]").format(new Date());
     }
 
 
